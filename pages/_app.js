6b4198deed01aec0e010e0c61f7e939158402d47
Manikanta Pindi <131873708+manikanta-pindi@users.noreply.github.com>
2024-02-07 22:43:59 +0530

    added react helmet

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { HelmetProvider } from "react-helmet-async";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const helmetContext = {};
+  return (
+    <HelmetProvider context={helmetContext}>
+      <Component {...pageProps} />
+    </HelmetProvider>
+  );
 }
